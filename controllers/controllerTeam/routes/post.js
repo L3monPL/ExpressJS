6aab2cae223bb11a2c9f53e7acc33a6c38d5d3d9
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2022-12-17 13:39:40 +0100

    change add team to match

--- controllers/controllerTeam/routes/post.js
+++ controllers/controllerTeam/routes/post.js
@@ -35,24 +35,41 @@ route.post("/create/:matchId", [
     var inputData1
     var inputData2
 
+    let current_status
+
     try {
 
-        //TEAM 1
-        await dbc.run(db, 'INSERT INTO team (name,created_at) VALUES (?,?)', [name_team_1, created_at])
-        let row = await dbc.get(db, 'SELECT last_insert_rowid() as id', [])
-        team_1 = row['id']
-        inputData1 = [team_1, matchId]
-        console.log(inputData1)
-        await dbc.run(db,`UPDATE match SET team_1_id = ? WHERE id = ?`,inputData1)
-        console.log[team_1]
+        let status_row = await dbc.get(db, 'SELECT * FROM match WHERE id = ?', [matchId])
+        current_status = status_row['status']        
 
-        //TEAM 2
-        await dbc.run(db, 'INSERT INTO team (name,created_at) VALUES (?,?)', [name_team_2, created_at])
-        row2 = await dbc.get(db, 'SELECT last_insert_rowid() as id', [])
-        team_2 = row2['id']
-        inputData2 = [team_2, matchId]
-        console.log(inputData2)
-        await dbc.run(db,`UPDATE match SET team_2_id = ? WHERE id = ?`,inputData2)
+        // console.log(current_status)
+
+
+        if (current_status !== 'Oczekuje na dodanie graczy') {
+            //TEAM 1
+            await dbc.run(db, 'INSERT INTO team (name,created_at) VALUES (?,?)', [name_team_1, created_at])
+            let row = await dbc.get(db, 'SELECT last_insert_rowid() as id', [])
+            team_1 = row['id']
+            inputData1 = [team_1, matchId]
+            console.log(inputData1)
+            await dbc.run(db,`UPDATE match SET team_1_id = ? WHERE id = ?`,inputData1)
+            console.log[team_1]
+
+            //TEAM 2
+            await dbc.run(db, 'INSERT INTO team (name,created_at) VALUES (?,?)', [name_team_2, created_at])
+            row2 = await dbc.get(db, 'SELECT last_insert_rowid() as id', [])
+            team_2 = row2['id']
+            inputData2 = [team_2, matchId]
+            console.log(inputData2)
+            await dbc.run(db,`UPDATE match SET team_2_id = ? WHERE id = ?`,inputData2)
+            inputData3 = ["Oczekuje na dodanie graczy", matchId]
+            await dbc.run(db,`UPDATE match SET status = ? WHERE id = ?`,inputData3)
+        }
+        else{
+            
+        }
+
+        
 
 
     }catch (err) {
@@ -66,7 +83,7 @@ route.post("/create/:matchId", [
         "data": matchId,
         "id" : this.lastID
     })
-    db.close()
+    // db.close()
 })
 
 
