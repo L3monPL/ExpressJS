656f10097905543d700b8f2e55071c221f3b8717
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2022-12-22 15:50:19 +0100

    change user select

--- controllers/controllerUser/routes/getList.js
+++ controllers/controllerUser/routes/getList.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const sqlite3 = require("sqlite3")
 const db = new sqlite3.Database("./data.db")
-const sql = "SELECT * FROM user"
+const sql = "SELECT id, username, email, created_at FROM user"
 
 router.get("", (req, res, next) => {
     db.all(sql, (err, rows) => {
